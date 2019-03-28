2cbdab387fa17edf160b93f33035f2b037d5d342
Harivansh Mathur <mathurharivansh811@gmail.com>
2019-03-28 10:44:24 +0530

    Made Subtraction Function

--- calculator.cpp
+++ calculator.cpp
@@ -13,17 +13,32 @@ class Calculator
                         c=a+b;
                         cout<<"Addition is: "<<c<<endl;
 		}
+		void Subtraction
+		{
+			int a,b,c;
+			cout<<"Enter First Number: "<<endl;
+			cin>>a;
+			cout<<"Enter Second Number: "<<endl;
+			cin>>b;
+			c=a-b;
+			cout<<"Subtraction is: "<<endl;
+		}
+
 };
 int main()
 {
 	Calculator c1;
 	int ch;
 	cout<<"Press 1 for Addition"<<endl;
+	cout<<"Press 2 for Subtraction"<<endl;
 	cin>>ch;
 	switch(ch)
 	{
 		case 1:
 			c1.Addition();
+			break;
+		case 2:
+			c1.Subtraction();
 			break;
 		default :
 			cout<<"Wrong Choice Entered"<<endl;
